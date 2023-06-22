38d33cc67cb3ad2ba3b84df330798b4870a6ef6c
Abdul Wadud Chowdhury <wadud@eskimi.com>
2023-06-22 13:49:43 +0600

    keywords column added.

--- app.js
+++ app.js
@@ -24,15 +24,15 @@ app.post('/fetch-url', async (req, res) => {
     }
 });
 
-app.post('/fetch-gpt-response', async (req, res) => {
+app.post('/fetch-iab-categories', async (req, res) => {
     const apiKey = process.env.GPT_API_KEY;
     const openAiApiUrl = 'https://api.openai.com/v1/chat/completions';
     const postData = {
-        "model": "gpt-3.5-turbo-0301",
+        "model": "gpt-3.5-turbo",
         "messages": [
             {
                 "role": "system",
-                "content": "Act as an Ad-tech expert. read the content and match IAB category or subcategory title for the content. if available, sub-category is preferable. respond only title. Example: Soccer/Football"
+                "content": "Act as an Ad-tech expert. read the content and match IAB category or subcategory title for the content. if available, sub-category is preferable. respond only title and code with a specific format. Format Example: IAB17-12: Football; IAB19-18: Internet Technology"
             },
             {
                 "role": "user",
@@ -66,12 +66,54 @@ app.post('/fetch-gpt-response', async (req, res) => {
     }
 });
 
+app.post('/fetch-keywords', async (req, res) => {
+    const apiKey = process.env.GPT_API_KEY;
+    const openAiApiUrl = 'https://api.openai.com/v1/chat/completions';
+    const postData = {
+        "model": "gpt-3.5-turbo-0301",
+        "messages": [
+            {
+                "role": "system",
+                "content": "Give me few keywords from this text, this text has one main topic and few small topics. please consider the main topic only, ignore small ones. example: anti-vaccine views, Turkey, economic policies"
+            },
+            {
+                "role": "user",
+                "content": req.body.content,
+            }
+        ]
+    };
+
+    try {
+        const response = await axios.post(openAiApiUrl, postData,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${apiKey}`
+                }
+            }
+        );
+
+        const data = response.data;
+
+        if (response.status === 200) {
+            const categoryData = extractKeywords(data.choices[0]['message']['content']);
+            res.send(categoryData);
+        } else {
+            console.error('Error:', data.error.message);
+            res.status(500).send('An error occurred while fetching response from open AI.');
+        }
+    } catch (error) {
+        console.error(`Error: ${error}`);
+        res.status(500).send('An error occurred while fetching response from open AI.');
+    }
+});
+
 function extractCategories(categoryData) {
-    return categoryData;
-    // Implement your logic to extract categories from the response text
-    // and return them as an array
-    // Example:
-    return ['Category 1', 'Category 2', 'Category 3'];
+    return categoryData.split(';').map(item => item.trim()).filter(item => item.length);
+}
+
+function extractKeywords(categoryData) {
+    return categoryData.split(',').map(item => item.trim()).filter(item => item.length);
 }
 
 app.listen(process.env.PORT || 3003, () => console.log('Server running on port 3003'));
